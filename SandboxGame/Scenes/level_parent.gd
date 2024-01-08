5ff45a4cf1347aff4a4a11b477da919fdd4f0fa5
NicholasWBarnes <nicholas.w.barnes@gmail.com>
2024-01-07 19:05:57 -0700

    Template Creation
    Created some templates, in addition to some other things.

--- SandboxGame/Scenes/level_parent.gd
+++ SandboxGame/Scenes/level_parent.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name LevelParent
 
+# Add code for preloading scenes and instantiating stuff here
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
